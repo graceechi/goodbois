3c65f5ea71391a0c5dcf7dee0eff9a90c70365ee
Grace Chi <gracetrc7@gmail.com>
2022-05-09 18:24:10 -0500

    created GET/POST signup routes and form

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,81 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { csrfProtection, asyncHandler } = require('./utils');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const db = require('../db/models');
+
+router.get('/signup', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('sign-up', {
+    title: 'Sign-Up',
+    user,
+    csrfToken: req.csrfToken(),
+  })
+})
+
+const userValidators = [
+  check('fullName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Full Name')
+    .isLength({ max: 100 })
+    .withMessage('Full Name must not be more than 100 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    // regex
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
+];
+
+router.post('/signup', csrfProtection, userValidators, asyncHandler(async(req, res) => {
+  const { fullName, email, password } = req.body;
+  const user = db.User.build({
+    fullName,
+    email
+  });
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
+    await user.save();
+    // persists the user's login state after successfully logging in
+    // loginUser(req, res, user);
+    res.redirect('/');
+    } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('sign-up', {
+        title: 'Sign Up',
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+    });
+  }
+
+}))
 
 module.exports = router;
