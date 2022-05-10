f22b03a27b3271575d390a54845f6e92b394e378
Sona Jasani <jasanisona@gmail.com>
2022-05-10 16:33:25 -0400

    user profile .js and .pug

--- routes/profile.js
+++ routes/profile.js
@@ -2,15 +2,27 @@ const express = require('express')
 const router = express.Router();
 
 const { csrfProtection, asyncHandler } = require('./utils');
+const { restoreUser } = require('../auth');
 const db = require('../db/models');
 
 
-router.get('/profile', csrfProtection, (req, res) => {
+router.get('/profile/:id', csrfProtection, restoreUser, asyncHandler(async(req, res) => {
+  const id = req.params.id;
+  const user = await db.User.findByPk(id);
+  const parkslist = await db.ParksList.findAll({
+    where : { userId : id },
+    include: [Parks],
+    order: {name},
+  });
     res.render('user-profile', {
       title: 'profile',
+      fullName: 'user.fullName',
+      email: 'user.email',
+      user,
+      parkslist,
       csrfToken: req.csrfToken(),
     });
-  });
+  }));
 
 
 
