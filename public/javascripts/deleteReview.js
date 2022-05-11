8c8f2807961760b93564021d40a6139a4b963abb
olivia <oliviabir19@gmail.com>
2022-05-11 11:11:58 -0700

    delete review progress

--- public/javascripts/deleteReview.js
+++ public/javascripts/deleteReview.js
@@ -1,21 +1,22 @@
-const deleteBtns = document.querySelectorAll('.delete-review-button')
+window.addEventListener('DOMContentLoaded', (event) => {
+    const deleteBtns = document.querySelectorAll('.delete-review-button')
 
-for (let i = 0; i < deleteBtns.length; i++) {
-    const btn = deleteBtns[i];
-    btn.addEventListener('click', async(e) => {
-        e.preventDefault()
-        const reviewId = e.target.id.split('-')[2]
-        // const parkId = **************************************FIGURE THIS OUT*******************
-        const res = await fetch(`/${parkId}/review/${reviewId}`, {
-            method: 'DELETE'
+    for (let i = 0; i < deleteBtns.length; i++) {
+        const btn = deleteBtns[i];
+        btn.addEventListener('click', async(e) => {
+            e.preventDefault()
+            const reviewId = e.target.id
+            const res = await fetch(`/${reviewId}`, {
+                method: 'DELETE'
+            })
+
+            const data = await res.json()
+            if (data.message === 'Success') {
+                const container = document.getElementById(`${review.id}-review-container`)
+                container.remove()
+            } else {
+
+            }
         })
-
-        const data = await res.json()
-        if (data.message === 'Success') {
-            const container = document.getElementById(`${review.id}-review-container`)
-            container.remove()
-        } else {
-
-        }
-    })
-}
+    }
+})
