160c5d0a3531c286847f82bb66ce48b5bf22c081
olivia <oliviabir19@gmail.com>
2022-05-11 10:46:18 -0700

    delete review progress

---
+++ public/javascripts/deleteReview.js
@@ -0,0 +1,21 @@
+const deleteBtns = document.querySelectorAll('.delete-review-button')
+
+for (let i = 0; i < deleteBtns.length; i++) {
+    const btn = deleteBtns[i];
+    btn.addEventListener('click', async(e) => {
+        e.preventDefault()
+        const reviewId = e.target.id.split('-')[2]
+        // const parkId = **************************************FIGURE THIS OUT*******************
+        const res = await fetch(`/${parkId}/review/${reviewId}`, {
+            method: 'DELETE'
+        })
+
+        const data = await res.json()
+        if (data.message === 'Success') {
+            const container = document.getElementById(`park-review-container`)
+            container.remove()
+        } else {
+
+        }
+    })
+}
