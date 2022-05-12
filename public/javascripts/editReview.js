04ebff04522d9459fea1fded809779b0abe28b5b
olivia <oliviabir19@gmail.com>
2022-05-12 13:50:59 -0700

    progress on dynamic edit

--- public/javascripts/editReview.js
+++ public/javascripts/editReview.js
@@ -5,6 +5,7 @@ for(let i = 0; i < editButtons.length; i++) {
 
     button.addEventListener('click', (e) => {
         const reviewId = e.target.id.split('-')[2]
+        const parkId = e.target.id.split('-')[3]
         const form = document.getElementById(`edit-container-${reviewId}`)
 
         if (form.classList.contains('hidden')) {
@@ -16,6 +17,24 @@ for(let i = 0; i < editButtons.length; i++) {
         const updateButton = document.getElementById(`submit-edit-${reviewId}`)
         updateButton.addEventListener('click', async(e) => {
             e.preventDefault();
+            const rating = document.getElementById(`rating-${review.id}`).value
+            const title = document.getElementById(`title-${review.id}`).value
+            const body = document.getElementById(`description-${review.id}`).value
+
+            const res = await fetch(`${parkId}/reviews/${reviewId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    rating,
+                    title,
+                    body
+                })
+            })
+
+            const data = await res.json()
+            if (data.message === 'Success') {
+
+            }
         })
     })
 }
