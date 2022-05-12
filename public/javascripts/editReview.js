5447b7e6d27632e36b8ae5ddadb19e2af3721223
olivia <oliviabir19@gmail.com>
2022-05-12 15:30:01 -0700

    edit reviews completed

--- public/javascripts/editReview.js
+++ public/javascripts/editReview.js
@@ -1,27 +1,26 @@
 const editButtons = document.querySelectorAll('.edit-buttons')
 
-for(let i = 0; i < editButtons.length; i++) {
+for (let i = 0; i < editButtons.length; i++) {
     const button = editButtons[i];
 
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', async(e) => {
+        e.preventDefault()
         const reviewId = e.target.id.split('-')[2]
         const parkId = e.target.id.split('-')[3]
-        const form = document.getElementById(`edit-container-${reviewId}`)
+        const form = document.getElementById('edit-container')
 
-        if (form.classList.contains('hidden')) {
-            form.classList.remove('hidden')
-        } else {
-            form.classList.add('hidden')
-        }
+        form.classList.add('active')
 
-        const updateButton = document.getElementById(`submit-edit-${reviewId}`)
+        const updateButton = document.getElementById(`submit-edit-${reviewId}-${parkId}`)
+
         updateButton.addEventListener('click', async(e) => {
             e.preventDefault();
-            const rating = document.getElementById(`rating-${review.id}`).value
-            const title = document.getElementById(`title-${review.id}`).value
-            const body = document.getElementById(`description-${review.id}`).value
 
-            const res = await fetch(`${parkId}/reviews/${reviewId}`, {
+            const rating = document.getElementById(`rating-${reviewId}`).value
+            const title = document.getElementById(`title-${reviewId}`).value
+            const body = document.getElementById(`description-${reviewId}`).value
+
+            const res = await fetch(`/parks/${parkId}/review/${reviewId}`, {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
@@ -33,8 +32,66 @@ for(let i = 0; i < editButtons.length; i++) {
 
             const data = await res.json()
             if (data.message === 'Success') {
-
+                const ratingEle = document.getElementById(`rating-${reviewId}-display`)
+                const titleEle = document.getElementById(`title-${reviewId}-display`)
+                const descriptionEle = document.getElementById(`description-${reviewId}-display`)
+                ratingEle.innerHTML = data.review.rating
+                titleEle.innerHTML = data.review.title
+                descriptionEle.innerHTML = data.review.body
+                form.classList.remove('active')
             }
         })
     })
 }
+
+const cancel = document.getElementById('cancel-btn')
+
+cancel.addEventListener('click', async(e) => {
+    e.preventDefault()
+    const form = cancel.closest('#edit-container')
+    form.classList.remove('active')
+})
+
+
+// const editButtons = document.querySelectorAll('.edit-buttons')
+
+// for(let i = 0; i < editButtons.length; i++) {
+//     const button = editButtons[i];
+
+//     button.addEventListener('click', (e) => {
+//         const reviewId = e.target.id.split('-')[2]
+//         const parkId = e.target.id.split('-')[3]
+//         const form = document.getElementById(`edit-container-${reviewId}`)
+
+//         form.classList.add('active')
+
+//         const updateButton = document.getElementById(`submit-edit-${reviewId}`)
+//         updateButton.addEventListener('click', async(e) => {
+//             e.preventDefault();
+//             const rating = document.getElementById(`rating-${review.id}`).value
+//             const title = document.getElementById(`title-${review.id}`).value
+//             const body = document.getElementById(`description-${review.id}`).value
+
+//             const res = await fetch(`${parkId}/reviews/${reviewId}`, {
+//                 method: 'PUT',
+//                 headers: { 'Content-Type': 'application/json' },
+//                 body: JSON.stringify({
+//                     rating,
+//                     title,
+//                     body
+//                 })
+//             })
+
+//             const data = await res.json()
+//             if (data.message === 'Success') {
+//                 const ratingEle = document.getElementById(`rating-${reviewId}-display`)
+//                 const titleEle = document.getElementById(`title-${reviewId}-display`)
+//                 const descriptionEle = document.getElementById(`description-${reviewId}-display`)
+//                 ratingEle.innerHTML = data.review.rating
+//                 titleEle.innerHTML = data.review.title
+//                 descriptionEle.innerHTML = data.review.body
+//                 form.classList.add('hidden')
+//             }
+//         })
+//     })
+// }
