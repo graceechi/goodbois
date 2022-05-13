c3246b34490ae18231123c640bc57b2359212941
Sona Jasani <jasanisona@gmail.com>
2022-05-13 16:50:58 -0400

    delete park from parklist button

--- public/javascripts/addToList.js
+++ public/javascripts/addToList.js
@@ -5,7 +5,9 @@ for (let i = 0; i < addToListBtns.length; i++) {
 
     btn.addEventListener('click', async(e) => {
         e.preventDefault();
+
+        
         
         const parkId = e.target.id.split('-')[2]
-        const form = getElementById('')
+    })
 }
