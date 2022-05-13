841c85a1ce6baffd9a63bedb72a7baebef20cb0e
Grace Chi <gracetrc7@gmail.com>
2022-05-13 13:10:08 -0500

    edited profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { csrfProtection, asyncHandler } = require('./utils');
 const { restoreUser, requireAuth, logoutUser } = require('../auth');
-const {User, Park, ParksList, Review } = require('../db/models');
+const {User, Park, ParksList} = require('../db/models');
 const db = require('../db/models');
 // const {logoutUser} = require('../auth')
 
@@ -12,36 +12,35 @@ const db = require('../db/models');
 
 
 router.get('/:id', csrfProtection, requireAuth, asyncHandler(async(req, res) => {
-  const userId = req.params.id;
-  const user = await User.findByPk(userId);
-  const parks = await Park.findAll({
-    include: [{
-      model: ParksList,
-      where: {userId}
-    }]
- });
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+    const parks = await Park.findAll({
+      include: [{
+        model: ParksList,
+        where: {userId}
+      }]
+   });
 
 
-res.render('user-profile', {
-  title: 'profile',
-  fullName: 'user.fullName',
-  email: 'user.email',
-  user,
-  parks,
-  csrfToken: req.csrfToken(),
-});
-}));
-
-router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
-  const userId = req.params.id;
-  // const user = await User.findByPk(userId);
-
-  // const parksId = req.params.id;
-
-  const user = await db.User.findByPk(userId, {
-      include: ParksList
+   res.render('user-profile', {
+    title: 'profile',
+    fullName: 'user.fullName',
+    email: 'user.email',
+    user,
+    parks,
+    csrfToken: req.csrfToken(),
   });
+  }));
 
+  router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
+    const userId = req.params.id;
+    // const user = await User.findByPk(userId);
+
+    // const parksId = req.params.id;
+
+    const user = await db.User.findByPk(userId, {
+        include: ParksList
+    });
   res.render('user-profile', {
     title: 'profile',
     fullName: 'user.fullName',
