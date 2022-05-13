a4a101b6ac7f80d03bac553670fa93bb52b6c800
olivia <oliviabir19@gmail.com>
2022-05-13 10:10:11 -0700

    css for edit review popup

--- public/javascripts/deleteReview.js
+++ public/javascripts/deleteReview.js
@@ -10,6 +10,7 @@ for (let i = 0; i < deleteBtns.length; i++) {
     })
 }
 
+
 const cancel = document.getElementById('cancel')
 
 cancel.addEventListener('click', async(e) => {
@@ -19,13 +20,12 @@ cancel.addEventListener('click', async(e) => {
     popup.classList.remove('active')
 })
 
-
-const bananas = document.querySelectorAll('.delete-yes') //<<<---- could not find a relevant variable name it would let me use, banana === delete
-
+const bananas = document.querySelectorAll('.delete-yes')
+//^^^---- could not find a relevant variable name it would let me use, banana === delete
 for (let i = 0; i < bananas.length; i++) {
     const banana = bananas[i];
 
-    banana.addEventListener('click', async(e) => { //<<<---- have to reload page to delete more than one review
+    banana.addEventListener('click', async(e) => {
         e.preventDefault()
         const reviewId = e.target.id.split('-')[2]
         const parkId = e.target.id.split('-')[3]
