a19339644d9a103669af393ca73c31cc762fbed2
olivia <oliviabir19@gmail.com>
2022-05-15 11:48:11 -0700

    fixed cancel button on user profile

--- public/javascripts/deleteParkFromList.js
+++ public/javascripts/deleteParkFromList.js
@@ -7,19 +7,26 @@ for (let i = 0; i < deleteBtns.length; i++) {
         e.preventDefault()
         const popupId = e.target.id.split('-')[2];
         const popup = document.getElementById(`popup-${popupId}`);
-        // console.log(popup)
         popup.classList.add('active')
+
     })
 }
-const cancel = document.getElementById('cancel');
 
+const cancelBtns = document.querySelectorAll('.cancel');
 
-cancel.addEventListener('click', async(e) => {
-    e.preventDefault()
-    const popup = cancel.closest('.popup')
-    console.log(popup)
-    popup.classList.remove('active')
-})
+for (let i = 0; i < cancelBtns; i++) {
+    const cancel = cancelBtns[i];
+    console.log('------THIS IS THE CANCEL-------')
+    console.log(cancel)
+
+    cancel.addEventListener('click', async(e) => {
+        e.preventDefault()
+        const parksId = e.target.id.split('-')[1];
+        const popup = document.getElementById(`cancel-${parksId}`)
+        popup.classList.remove('active')
+    })
+}
+
 
 const yesBtns = document.querySelectorAll('.delete-yes');
 
